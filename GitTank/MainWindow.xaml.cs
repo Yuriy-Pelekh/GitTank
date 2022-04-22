0e20e26ecb9c24d32d925f05795f674bcedffb59
KolyaNu4 <mykola.andrietc@gmail.com>
2022-04-22 13:59:39 +0300

    Fix defects, deleting useless code

--- GitTank/MainWindow.xaml.cs
+++ GitTank/MainWindow.xaml.cs
@@ -25,16 +25,5 @@ namespace GitTank
         {
             (sender as TextBox).ScrollToEnd();
         }
-
-        /* private void OnButtonBranchesClick(object sender, RoutedEventArgs e)
-         {
-             ((UIElement) sender).IsEnabled = false;
-             TextBoxOutput.Text = string.Empty;
-             Task.Run(() =>
-             {
-                 var remoteBranches = _gitProcessor.Branches();
-                 Dispatcher.BeginInvoke(new Action(() => ((UIElement) sender).IsEnabled = true), DispatcherPriority.Background);
-             });
-         }*/
     }
 }
